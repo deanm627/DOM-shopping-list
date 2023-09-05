19943979d7bca244dc46cb3ffb683c81d41a382f
Mary Dean <marydean@Marys-MacBook-Air.local>
2023-09-05 14:31:42 -0400

    My version

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 'use strict';
+const button = document.querySelector("#generateList");
+const inputs = document.querySelectorAll(".input");
+const list = document.querySelectorAll(".listItem");
+
+function listGenerator() {
+    const inputArr = [];
+    inputs.forEach(function(userInput) {
+        inputArr.push(document.getElementById(userInput.id).value);
+    });
+    list.forEach(function(item, index) {
+        item.innerHTML = inputArr[index];
+    });
+    document.getElementById("form").reset();
+};
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    listGenerator();
+});
